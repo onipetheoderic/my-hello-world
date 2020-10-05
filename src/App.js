2100bb81da13365c5684a035d9c53177257f9a7e
onipe theoderic <onipetheoderic@gmail.com>
2020-10-05 14:31:22 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 export default () => (
   <div className="App">
     <header className="App-header">
-      <h1 className="App-title">Welcome To My Hello World!</h1>
+      <h1 className="App-title">Welcome to Theoderic apps</h1>
     </header>
     <p className="App-intro">
-      My name is Theoderic onipe Welcome, and hello!
+      My name is Theoderic onipe
     </p>
   </div>
 );
