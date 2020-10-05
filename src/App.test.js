853823455ce31884048e1be97ea058559708e142
onipe theoderic <onipetheoderic@gmail.com>
2020-10-05 14:38:39 +0100

    corrected the test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('expects my name is theoderic to be there', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/theoderic/i);
+  const linkElement = getByText(/my name is theoderic/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('expects to see the welcome text', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Welcome to Theoderic apps/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
